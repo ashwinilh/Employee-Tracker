71897cdbac6cf8c9a7b3429a1f4158c137a85267
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-10-17 20:25:10 -0500

    adding db queries

--- db/index.js
+++ db/index.js
@@ -1,18 +1,58 @@
 const connection = require('./connection');
 const { prompt } = require("inquirer");
-function init() {
-    console.log(“Employees, Departments, and Roles Interface”)
-    loadPrompts();
-   }
-   
-   closeConnection(){
-    try{
-        this.connection.end();
+class DB {
+
+    constructor(connection) {
+        this.connection = connection;
     }
-    catch(error){
-        console.log("Error closing connection : "+error);
-    }      
-}
+
+    // view all employees by deparment
+    viewEmployeesByDepartment() {
+        return this.connection.query(
+            `SELECT
+                e1.id AS ID,
+                e1.first_name AS First_Name,
+                e1.last_name AS Last_Name,
+                role.title AS Role,
+                department.name AS Department,
+                CONCAT(e2.first_name, ' ', e2.last_name) AS Manager,
+                role.salary AS Salary
+            FROM
+                employee e1
+            LEFT JOIN
+                role ON e1.role_id = role.id
+            LEFT JOIN
+                employee e2 ON e1.manager_id = e2.id
+            LEFT JOIN department ON role.department_id = department.id
+            ORDER BY
+                department.name;`
+        );
+    }
+
+    // View employee my manager.
+    viewAllEmployeesByManager() {
+        return this.connection.query(
+            `SELECT
+                e1.id AS ID,
+                e1.first_name AS First_Name,
+                e1.last_name AS Last_Name,
+                role.title AS Role,
+                department.name AS Department,
+                CONCAT(e2.first_name, ' ', e2.last_name) AS Manager,
+                role.salary AS Salary
+            FROM
+                employee e1
+            LEFT JOIN
+                role ON e1.role_id = role.id
+            LEFT JOIN
+                employee e2 ON e1.manager_id = e2.id
+		    LEFT JOIN department ON role.department_id = department.id
+		    ORDER BY
+                e1.manager_id;`
+        );
+    }
+
+
 }
 
 module.exports = new DB(connection);
