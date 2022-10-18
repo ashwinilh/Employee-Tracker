e863478ae3e7fa8a8a1dd6535b56b936c4c48435
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-10-17 20:42:43 -0500

    employee schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,25 @@
 -- Drops/deletes database if already exists -- 
-DROP DATABASE IF EXISTS employee_DB;
+DROP DATABASE IF EXISTS employee_tracker_db;
 
--- Creates the "employee_DB" database --
-CREATE DATABASE employee_DB;
+-- Creates the "employee_tracker_db" database --
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+-- Create employee table --
+CREATE TABLE employee(
+	id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+	CONSTRAINT fk_role FOREIGN KEY (role_id) 
+    REFERENCES role(id) 
+		ON UPDATE CASCADE 
+        ON DELETE CASCADE,
+    CONSTRAINT fk_employee FOREIGN KEY (manager_id) 
+    REFERENCES employee(id) 
+		ON UPDATE CASCADE 
+        ON DELETE CASCADE,
+    PRIMARY KEY (id)
+);
