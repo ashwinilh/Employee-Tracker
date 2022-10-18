9401250734a27f34a1f1ec2207df7d76136e0261
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-10-17 21:19:42 -0500

    update queries

--- server.js
+++ server.js
@@ -30,9 +30,11 @@ async function mainPrompt() {
             break;
 
         case 'Update employee role':
+            updateEmployeeRole();
             break;
 
         case 'Update employee manager':
+            updateEmployeeManager();
             break;
 
         case 'Exit':
@@ -132,6 +134,94 @@ async function removeEmployee() {
 	const removeEmployeeResult = await db.removeEmployee(empId);
 	viewAllEmployees();
 }
+
+// Update employee role
+async function updateEmployeeRole() {
+	const rolesResult = await db.getRoles();
+	let roleNames = [];
+	for (let i = 0; i < rolesResult.length; i++) {
+		roleNames.push(rolesResult[i].title);
+	}
+	const employeeResult = await db.getEmployees();
+	let employeeNames = [];
+	for (let i = 0; i < employeeResult.length; i++) {
+		employeeNames.push(
+			employeeResult[i].first_name + " " + employeeResult[i].last_name
+		);
+	}
+	let updateEmpRole = [];
+	updateEmpRole.push({
+		type: "list",
+		name: "empName",
+		message: "Which employee's role to update?",
+		choices: employeeNames,
+	});
+	updateEmpRole.push({
+		type: "list",
+		name: "roleName",
+		message: "Which role to update for current employee?",
+		choices: roleNames,
+	});
+	const { empName, roleName } = await inquirer.prompt(updateEmpRole);
+	const empFirstName = empName.split(" ")[0];
+	const empLastName = empName.split(" ")[1];
+	const empId = employeeResult.filter(
+		(employee) =>
+			employee.first_name === empFirstName &&
+			employee.last_name === empLastName
+	)[0].id;
+	const roleId = rolesResult.filter((role) => role.title === roleName)[0].id;
+	const updateRoleResult = await db.updateEmployeeRole(empId, roleId);
+	viewAllEmployees();
+}
+
+// Update employee manager
+async function updateEmployeeManager() {
+	const employeeResult = await db.getEmployees();
+	let employeeNames = [];
+	for (let i = 0; i < employeeResult.length; i++) {
+		employeeNames.push(
+			employeeResult[i].first_name + " " + employeeResult[i].last_name
+		);
+	}
+	let updateEmpManager = [];
+	updateEmpManager.push({
+		type: "list",
+		name: "empName",
+		message: "Which employee's manager to update?",
+		choices: employeeNames,
+	});
+	const { empName } = await inquirer.prompt(updateEmpManager);
+	const empNames = employeeNames.filter((employee) => employee !== empName);
+	updateEmpManager.push({
+		type: "list",
+		name: "managerName",
+		message: "Who is employee's new manager?",
+		choices: empNames,
+	});
+	const { managerName } = await inquirer.prompt(updateEmpManager[1]);
+	const empFirstName = empName.split(" ")[0];
+	const empLastName = empName.split(" ")[1];
+	const managerFirstName = managerName.split(" ")[0];
+	const managerLastName = managerName.split(" ")[1];
+	const empId = employeeResult.filter(
+		(employee) =>
+			employee.first_name === empFirstName &&
+			employee.last_name === empLastName
+	)[0].id;
+	const managerId = employeeResult.filter(
+		(employee) =>
+			employee.first_name === managerFirstName &&
+			employee.last_name === managerLastName
+	)[0].id;
+	const updatemanagerResult = await db.updateEmployeeManager(
+		empId,
+		managerId
+	);
+
+	viewAllEmployees();
+}
+
 
 function init() {
 
